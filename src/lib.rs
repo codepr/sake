091dff00ab670e3a4761da081d0c43fb3fc54435
Andrea <a.g.baldan@gmail.com>
2023-02-13 23:24:01 +0000

    Removed unused dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,1 @@
 pub mod mqtt;
-
-pub type AsyncResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-pub type SerdeResult<T> = std::result::Result<T, Box<bincode::ErrorKind>>;
